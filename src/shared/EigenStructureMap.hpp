589600e0dc6326c59eebb0c7b221831c1527d94b
Matteo Chen <kaiximatteo.chen@mail.polimi.it>
2024-01-20 14:57:22 +0100

    Added support for sparse matrices

--- src/shared/EigenStructureMap.hpp
+++ src/shared/EigenStructureMap.hpp
@@ -10,6 +10,7 @@
 
 #include <Eigen/Dense>
 #include <cstddef>
+#include <cstdint>
 #include <type_traits>
 
 /*!
@@ -48,9 +49,19 @@ class EigenStructureMap {
                                                                    cols);
   }
 
+  template <typename IndexType>
+  static EigenStructureMap<EigenStructure, Scalar, MappedMatrix> create_map(
+      const std::size_t rows, const std::size_t cols, std::size_t nnz,
+      IndexType *outer_size_ptr, IndexType *inner_size_ptr, Scalar *value_ptr) {
+    return EigenStructureMap<EigenStructure, Scalar, MappedMatrix>(
+        rows, cols, nnz, outer_size_ptr, inner_size_ptr, value_ptr);
+  }
+
+  // Unsafe direct usage, make attention, prefer static constructor
   EigenStructureMap(Scalar *data, const std::size_t size)
       : structure_map(data, size) {}
 
+  // Unsafe direct usage, make attention, prefer static constructor
   EigenStructureMap(Scalar *data, const std::size_t rows,
                     const std::size_t cols)
       : structure_map(data, rows, cols) {}
@@ -59,6 +70,13 @@ class EigenStructureMap {
 
  protected:
   Eigen::Map<EigenStructure> structure_map;
+
+  template <typename IndexType>
+  EigenStructureMap(uint32_t rows, uint32_t cols, uint32_t nnz,
+                    IndexType *outer_size_ptr, IndexType *inner_size_ptr,
+                    Scalar *value_ptr)
+      : structure_map(rows, cols, nnz, outer_size_ptr, inner_size_ptr,
+                      value_ptr) {}
 };
 
 #endif  // EIGEN_MATRIX_MAP_HPP
