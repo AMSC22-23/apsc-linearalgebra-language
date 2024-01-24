92bcc7cb2aa0f66db50cd03c70b60b2e200f2c95
Matteo Chen <kaiximatteo.chen@mail.polimi.it>
2024-01-24 14:29:59 +0100

    Enhanced variable naming

--- src/shared/MPISparseMatrix.hpp
+++ src/shared/MPISparseMatrix.hpp
@@ -74,7 +74,7 @@ class MPISparseMatrix {
     bool dead_signal = 0;
     if constexpr (std::is_base_of_v<Eigen::SparseCompressedBase<Matrix>,
                                     Matrix>) {
-      if (mpi_rank == 0 && !compressed_global_sparse_matrix.isCompressed()) {
+      if (mpi_rank == manager && !compressed_global_sparse_matrix.isCompressed()) {
         dead_signal = 1;
       }
       MPI_Bcast(&dead_signal, 1, mpi_typeof(decltype(dead_signal){}), 0,
